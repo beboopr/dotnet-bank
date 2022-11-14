b8e1c0dae5be402c36119d2078e86c33bea7acb7
beboopr <linnets-router.09@icloud.com>
2022-11-14 15:03:54 -0500

    bank done

---
+++ BankAccount.cs
@@ -0,0 +1,38 @@
+class BankAccount{
+   public string accountNumber {get; set;}
+    // public decimal balance {get; set;}
+    private decimal balance {get; set;}
+
+    public decimal getBalance() {
+      return balance;
+    }
+    public void makeDeposit(decimal newDeposit){
+        if (newDeposit < 0 ) {
+          // balance = balance;
+            throw new Exception("deposit was under 0 dollars");
+        } else {
+        balance += newDeposit;
+        }
+    } 
+
+    // constructor
+    public BankAccount() {
+      accountNumber = "unknown";
+      balance = 100.01m;
+    }
+
+    public BankAccount(string newAccountName) {
+      accountNumber = newAccountName;
+      balance = 100.01m;
+    }
+    public BankAccount(decimal startingBalance) {
+        accountNumber = "unknown";
+        balance = 100.01m;
+
+        if ( startingBalance > 10000 ) {
+          balance = startingBalance + 2000;
+        } else {
+          balance = startingBalance + 100.01m;
+        }
+    }
+}
--- program.cs
+++ program.cs
@@ -1,1 +1,13 @@
-Console.WriteLine("hello world");
+Console.WriteLine("hello to the Bank");
+
+BankAccount account = new BankAccount(1100);
+
+// account.balance = 100;
+   try{
+    account.makeDeposit(-1);
+   } catch(Exception ex) {
+    Console.WriteLine(ex.Message);
+   }
+
+Console.Write($" Your account balance is: {account.getBalance()}");
+Console.WriteLine($" AccountNumber: {account.accountNumber}");
